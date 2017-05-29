053485b275c6c430b47dc0c846a1ac4f6bcbba6b
Ignacio Toledo <ignacio@ign.uy>
2017-05-29 14:20:08 -0300

    Feeds se actualizan una vez por hora

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('feeds:fetch')->everyFiveMinutes();
+		$schedule->command('feeds:fetch')->hourly();
 	}
 
 }
